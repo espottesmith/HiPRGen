39b50fd32f920a007bc0dd913164d94108faca29
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2021-08-02 13:53:28 -0700

    Bunch of changes

--- HiPRGen/species_filter.py
+++ HiPRGen/species_filter.py
@@ -105,7 +105,7 @@ def species_filter(
         mol_entries_unfiltered,
         species_report,
         mol_pictures_folder_name='mol_pictures_unfiltered',
-        rebuild_mol_pictures=True
+        rebuild_mol_pictures=generate_unfiltered_mol_pictures
     )
 
     report_generator.emit_text("species report")
